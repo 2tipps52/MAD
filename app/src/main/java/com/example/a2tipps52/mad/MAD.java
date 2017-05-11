b262097efab360a1e582607b621ffb3e5a0a25d0
2tipps52 <sam.tipping@gmail.com>
2017-05-11 15:18:31 +0100

    finl2

--- app/src/main/java/com/example/a2tipps52/mad/MAD.java
+++ app/src/main/java/com/example/a2tipps52/mad/MAD.java
@@ -1,6 +1,7 @@
 package com.example.a2tipps52.mad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -71,10 +72,15 @@ public class MAD extends AppCompatActivity {
                 System.out.println("Could not save a place" + e);
             }
         }
+        if (item.getItemId() == R.id.prefs) {
+            Intent intent = new Intent(this, Prefs.class);
+            startActivityForResult(intent, 1);
+            return true;
+        }
         return false;
     }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
                 Bundle bundle = intent.getExtras();
                 String name = bundle.getString("com.example.naem");
@@ -86,8 +92,22 @@ public class MAD extends AppCompatActivity {
                 OverlayItem AnItem = new OverlayItem(name, description, new GeoPoint(latttttt, longgggg));
                 items.addItem(AnItem);
             }
-        }
 
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Configuration.getInstance().load(this,
+                PreferenceManager.getDefaultSharedPreferences(this));
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Boolean preference = prefs.getBoolean("miccheck", false);
+        if (preference == false){
+            Toast.makeText(this, "no", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "yes", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
